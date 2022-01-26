95029533218df330428a8fe51f24f4b386e7ffc2
Anna <anna.foerster1@gmx.net>
2022-01-26 09:24:18 +0100

    added gartenmagazin

--- config/initializers/SecureHeaders.rb
+++ config/initializers/SecureHeaders.rb
@@ -8,7 +8,8 @@ SecureHeaders::Configuration.default do |config|
   }
 
 	config.csp = {
-		default_src: %w('self' https://www.openstreetmap.org/),
+		default_src: %w('self'),
+		frame_src: %w('self' https://azana.de https://www.openstreetmap.org/),
 		font_src: %w('self' https://fonts.googleapis.com https://fonts.gstatic.com),
 		script_src: %w('self' 'unsafe-inline'),
 		style_src_elem: %w('self' https://fonts.googleapis.com https://cdn.jsdelivr.net/ 'unsafe-inline' ),
