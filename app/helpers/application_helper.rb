fe91b91fd5964aa5d181d790b901367d567ca69c
Anna <anna.foerster1@gmx.net>
2022-04-04 13:50:12 +0200

    added comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
+	# Creates a custom title for each page
 	def title(page_title)
 		base_title = "Gartenbau Schneider"
 		page_title.empty? ? base_title : page_title + " | " + base_title
